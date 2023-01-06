8a63e3b6da880c84649690e380654066deae02db
Slomo Chacko <96213175+schacko9@users.noreply.github.com>
2023-01-06 12:58:52 -0500

    Serverless SQL Pool

---
+++ sql_scripts/nyc_taxi/3.setup/12_create_silver_trip_data_green.sql
@@ -0,0 +1,22 @@
+USE nyc_taxi_ldw;
+
+
+-- Stored Procedure 
+EXEC silver.sp_silver_trip_data_green '2020', '01'
+EXEC silver.sp_silver_trip_data_green '2020', '02'
+EXEC silver.sp_silver_trip_data_green '2020', '03'
+EXEC silver.sp_silver_trip_data_green '2020', '04'
+EXEC silver.sp_silver_trip_data_green '2020', '05'
+EXEC silver.sp_silver_trip_data_green '2020', '06'
+EXEC silver.sp_silver_trip_data_green '2020', '07'
+EXEC silver.sp_silver_trip_data_green '2020', '08'
+EXEC silver.sp_silver_trip_data_green '2020', '09'
+EXEC silver.sp_silver_trip_data_green '2020', '10'
+EXEC silver.sp_silver_trip_data_green '2020', '11'
+EXEC silver.sp_silver_trip_data_green '2020', '12'
+EXEC silver.sp_silver_trip_data_green '2021', '01'
+EXEC silver.sp_silver_trip_data_green '2021', '02'
+EXEC silver.sp_silver_trip_data_green '2021', '03'
+EXEC silver.sp_silver_trip_data_green '2021', '04'
+EXEC silver.sp_silver_trip_data_green '2021', '05'
+EXEC silver.sp_silver_trip_data_green '2021', '06'
---
+++ sql_scripts/nyc_taxi/3.setup/14_create_gold_trip_data_green.sql
@@ -0,0 +1,22 @@
+USE nyc_taxi_ldw;
+
+-- Stored Procedures 
+EXEC gold.sp_gold_trip_data_green '2020', '01';
+EXEC gold.sp_gold_trip_data_green '2020', '02';
+EXEC gold.sp_gold_trip_data_green '2020', '03';
+EXEC gold.sp_gold_trip_data_green '2020', '04';
+EXEC gold.sp_gold_trip_data_green '2020', '05';
+EXEC gold.sp_gold_trip_data_green '2020', '06';
+EXEC gold.sp_gold_trip_data_green '2020', '07';
+EXEC gold.sp_gold_trip_data_green '2020', '08';
+EXEC gold.sp_gold_trip_data_green '2020', '09';
+EXEC gold.sp_gold_trip_data_green '2020', '10';
+EXEC gold.sp_gold_trip_data_green '2020', '11';
+EXEC gold.sp_gold_trip_data_green '2020', '12';
+EXEC gold.sp_gold_trip_data_green '2021', '01';
+EXEC gold.sp_gold_trip_data_green '2021', '02';
+EXEC gold.sp_gold_trip_data_green '2021', '03';
+EXEC gold.sp_gold_trip_data_green '2021', '04';
+EXEC gold.sp_gold_trip_data_green '2021', '05';
+EXEC gold.sp_gold_trip_data_green '2021', '06';
+
